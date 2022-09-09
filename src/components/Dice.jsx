371c18969cec3f3dde2349ac08336f7eb729fd00
Harsh Prajapati <golaharsh2001@gmail.com>
2022-09-09 20:42:27 +0530

    added dots instead of numeric values (+ css)

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -6,13 +6,80 @@ export default function Dice(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
 
+    const face = [];
+    switch (props.value) {
+        case 1:
+            face.push(<div className="dice first-face">
+                <span class="dot"> </span>
+            </div>)
+            break;
+        case 2:
+            face.push(<div className="dice second-face">
+                <span class="dot"> </span>
+                <span class="dot"> </span>
+            </div>)
+            break;
+        case 3:
+            face.push(<div class="dice third-face">
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+            </div>)
+            break;
+        case 4:
+            face.push(<div class="fourth-face dice">
+                <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+                <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            </div>)
+            break;
+        case 5:
+            face.push(<div class="fivth-face dice">
+                <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+                <div className="column">
+                    <span class="dot"></span>
+                </div>
+                <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            </div>)
+            break;
+        case 6:
+            face.push(<div class="sixth-face dice">
+                <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+                <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            </div>)
+            break;
+
+        default:
+            break;
+    }
+
+
     return (
         <div
             onClick={() => props.toggle(props.id)}
-            className="oneDice"
             style={style}
+            className="diceDiv"
         >
-            <p>{props.value}</p>
+            {face}
         </div>
     )
 }
